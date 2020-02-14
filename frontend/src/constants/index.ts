5bee7b873c53cbe9cf675290a56ebd4ad1bc4ded
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-02-14 22:55:10 +1000

    UI: fix configmap API_URL

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,5 @@
 export const ENTER_KEY = 13;
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.REACT_APP_API_URL;
 
 console.debug(`Using api: ${API_URL}`);
+console.debug(`env:`, process.env);
