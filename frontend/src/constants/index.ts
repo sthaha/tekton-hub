101b198b1a3866f33bc4382323bfc7fbeb7cb6fa
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2020-02-14 22:14:32 +1000

    UI: debug API_URL being used

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,2 +1,4 @@
 export const ENTER_KEY = 13;
-export const API_URL = process.env.API_URL || 'https://api-tekton-hub.apps-crc.testing';
+export const API_URL = process.env.API_URL;
+
+console.debug(`Using api: ${API_URL}`);
