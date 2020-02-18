6cd21251edb50c06dcc196baeb2b7718d337215a
PuneetPunamiya <puneetpunamiya@gmail.com>
2020-02-18 19:16:52 +0530

    Optimized image for ui and avoided use of config map to get backend route

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,5 +1,6 @@
 export const ENTER_KEY = 13;
-export const API_URL = process.env.REACT_APP_API_URL;
+var api2 = window.location.host.replace(/^\w+-/, 'api-')
+export const API_URL = 'https://' + api2
 
 console.debug(`Using api: ${API_URL}`);
 console.debug(`env:`, process.env);
